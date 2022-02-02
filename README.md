ce9c7be70b3300ea58edc9244941f87abef80a97
Ankur Seth <iankurseth@gmail.com>
2022-02-03 03:07:40 +0530

    c0 with tailwind and cleanup

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Reactry's `github`
+A GitHub browser written in `React`.
+
+
+## `0.1.0` - `2022-02-03`
+* `cra`, `tailwind` and `cleanup`
+
+
---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+	return (
+		<div className="App">
+			<main className="min-h-screen bg-indigo-500 flex">
+				<h2 className="text-white text-4xl m-auto">Hallo Welt</h2>
+			</main>
+		</div>
+	);
+}
+
+export default App;
